bcb4b652a1e110786ec3846447b12efad7ea3ab4
Anthony Zardis <anthonyzardis@gmail.com>
2017-10-30 20:18:50 -0700

    Stacks/Queues

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -1,4 +1,4 @@
-class MyQueue
+class MyQueue 
   attr_accessor :head
   attr_accessor :tail
 
@@ -8,11 +8,17 @@ class MyQueue
   end
 
   def enqueue(element)
+    @queue.unshift(element)
+    @head = @queue[0]
+    @tail = @queue.last
   end
 
   def dequeue
+    @queue.slice!(@queue.lenght - 1)
+    @tails = @queue.last
   end
 
   def empty?
+    @queue.length == 0
   end
 end
