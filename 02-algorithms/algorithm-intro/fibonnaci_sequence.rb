6ba488b43f8c197f4af838a32e50044750799a5d
Anthony Zardis <anthonyzardis@gmail.com>
2017-11-27 12:15:46 -0800

    Intro Algorithms

---
+++ 02-algorithms/algorithm-intro/fibonnaci_sequence.rb
@@ -0,0 +1,20 @@
+def fib(n)
+	i = 0
+	j = 1
+
+	while i <= n
+		puts i
+		i, j = j, i + j
+	end
+end
+
+ puts fib(0)
+ puts fib(1)
+ puts fib(2)
+ puts fib(3)
+ puts fib(4)
+ puts fib(5)
+ puts fib(6)
+ puts fib(7)
+ puts fib(8)
+ puts fib(9)
